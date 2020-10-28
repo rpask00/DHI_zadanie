40895c18da8dc6ad4fad75bece37615641062c3a
Rafał <rp.261000@gmail.com>
2020-10-28 18:54:58 +0100

    src fix

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist/dhi'));
 
 app.get('/*', function (req, res) {
-    // res.sendFile('index.html', { root: 'dist/dhi/' });
-    res.sendFile(path.join(__dirname + '/dist/dhi/index.html'));
+    res.sendFile('index.html', { root: 'dist/dhi/' });
+    console.log(__dirname)
+    // res.sendFile(path.join(__dirname + '/dist/dhi/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
